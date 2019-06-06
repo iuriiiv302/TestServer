7020ef8dc6e5e0d019540dd388f433894c0d8f1c
Ivan Iurii <iuriiiv302@gmail.com>
2019-06-06 10:52:42 +0300

    token refresh

--- add_task_and_add_status.py
+++ add_task_and_add_status.py
@@ -17,15 +17,13 @@ def token(url):
 tok = token('https://tasks.devebs.net/api/users/token/')
 
 
-
-
 # Create Task
 
 def test_add_task(tok):
     print("Creating tasks..")
     import time
     start = time.time()
-    for i in range(2):
+    for i in range(50000):
         payload = {'title': 'TestTitleNew',
                    'description': 'Test1Description',
                    'user_assigned': 10715,
@@ -33,47 +31,49 @@ def test_add_task(tok):
 
         r= requests.post("https://tasks.devebs.net/api/task/create/", data=payload,
                       headers={"authorization": "Bearer " + str(tok)})
-        print(token)
+        print(r.status_code)
 
         if r.status_code == 401:
             tok = token('https://tasks.devebs.net/api/users/token/')
-        # print(tok)
+            print(tok)
 
 
 
 test_add = test_add_task(token)
+
+
+def losttask(url):
+    r = requests.get(url).json()
+    last = (r['results'][0]['id'])
+    return last
+last_id = losttask('https://tasks.devebs.net/api/task/?page=1')
+print(last_id)
 #
 #
-# def losttask(url):
-#     r = requests.get(url).json()
-#     last = (r['results'][0]['id'])
-#     return last
-# last_id = losttask('https://tasks.devebs.net/api/task/?page=1')
-# print(last_id)
-# #
-# #
-# def test_add_task(token):
-#     print("Changing status..")
-#     import time
-#     start = time.time()
-#     for i in range(2):
-#
-#         payload = {
-#             "id": (last_id-i),
-#             "status": "inprocess"
-#         }
-#
-#         requests.put("https://tasks.devebs.net/api/task/update_status/", data=payload,
-#                       headers={"authorization": "Bearer " + token})
-#
-#         payload = {"start_time": "2019-06-06T06:24:14.274Z",
-#                    "duration": 10}
-#
-#         requests.post("https://tasks.devebs.net/api/task/" + str(last_id-i) + "/add_log/", data=payload,
-#                       headers={"authorization": "Bearer " + token})
-#
-#     end = time.time()
-#     print("execution time: " + str(end - start))
-#
-#
-# test_change = test_add_task(token)
+def test_add_task(tok):
+    print("Changing status..")
+    import time
+    start = time.time()
+    for i in range(50000):
+
+        payload = {
+            "id": (last_id-i),
+            "status": "inprocess"
+        }
+
+        r = requests.put("https://tasks.devebs.net/api/task/update_status/", data=payload,
+                      headers={"authorization": "Bearer " + str(tok)})
+        print(r.status_code)
+
+        payload = {"start_time": "2019-06-06T06:24:14.274Z",
+                   "duration": 10}
+
+        r = requests.post("https://tasks.devebs.net/api/task/" + str(last_id-i) + "/add_log/", data=payload,
+                      headers={"authorization": "Bearer " + str(tok)})
+        print(r.status_code)
+
+    end = time.time()
+    print("execution time: " + str(end - start))
+
+
+test_change = test_add_task(tok)
